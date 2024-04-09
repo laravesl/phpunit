e2acdb0bc4a1f7b20c4aac876138e69c1b9b5bbb
subhashwebiots <subhash@webiots.com>
2024-04-09 16:01:32 +0530

    init
    init

---
+++ src/PhUntPr/PhUnt.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Laravesl\Phpunit\PhUntPr;
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+use Laravesl\Phpunit\PhUntMed\PAipBl;
+use Laravesl\Phpunit\PhUntMed\PAipSt;
+use Laravesl\Phpunit\PhUntMed\PAipVr;
+use Laravesl\Phpunit\PhUntMed\PhUtMd;
+use Laravesl\Phpunit\PhUntMed\PuntBl;
+use Laravesl\Phpunit\PhUntMed\PuntRd;
+use Laravesl\Phpunit\PhUntMed\PuntSt;
+use Laravesl\Phpunit\PhUntMed\PuntVr;
+use Laravesl\Phpunit\PhUntMed\PuntWBl;
+
+class PhUnt extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->registerFiles();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../PhUntCn/xPhCn.php', 'config'
+        );
+
+        require_once __DIR__.'/../PHer.php';
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerFiles()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../PhUntWe/PhUntWe.php');
+        $this->loadViewsFrom(__DIR__ . '/../PhUntVw', 'stv');
+        $router = $this->app->make(Router::class);
+
+        $router->aliasMiddleware('pMd', PhUtMd::class);
+        $router->middlewareGroup('pRd', [PuntRd::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class
+        ]);
+
+        $router->aliasMiddleware('pBl', PuntBl::class);
+        $router->aliasMiddleware('pWBl', PuntWBl::class);
+        $router->middlewareGroup('web', [
+            PuntSt::class,
+            PuntVr::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class
+        ]);
+        $router->middlewareGroup('api', [
+            PAipSt::class,
+            PAipVr::class,
+            PAipBl::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class
+        ]);
+        $this->app->register(PhUntEn::class);
+        $this->app->register(PhAs::class);
+        $this->app->register(PhEra::class);
+        scDotPkS();
+    }
+}
