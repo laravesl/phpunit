74faa96d6783c4a2d0d74eb65a041db64688eddc
slunit <envatoverify@webiots.com>
2025-02-04 18:14:18 +0530

    changes
    changes

--- src/PhUntMed/PuntVr.php
+++ src/PhUntMed/PuntVr.php
@@ -4,6 +4,7 @@ namespace Laravesl\Phpunit\PhUntMed;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 
 class PuntVr
 {
@@ -11,19 +12,31 @@ class PuntVr
    * Handle an incoming request.
    *
    * @return mixed
-    */
+   */
 
-    public function handle(Request $request, Closure $next)
-    {
-      if (schSync()) {
-        return to_route(xPhpLib('aW5zdGFsbC52ZXJpZnkuc2V0dXA='));
-      }
-
-      $response = $next($request);
-      if ($response instanceof \Symfony\Component\HttpFoundation\BinaryFileResponse) {
-        return $response;
-      }
-
-      return $next($request)->header('Cache-control', 'no-control, no-store, max-age=0, must-revalidate')->header('Pragma', 'no-cache')->header('Exprires', 'Sat 01 Jan 1990 00:00:00 GMT');
+  public function handle(Request $request, Closure $next)
+  {
+    if (schSync()) {
+      return to_route(xPhpLib('aW5zdGFsbC52ZXJpZnkuc2V0dXA='));
     }
+
+    try {
+
+      foreach ($request->allFiles() as $file) {
+        if ($file instanceof UploadedFile) {
+          return $next($request)->header('Cache-control', 'no-control, no-store, max-age=0, must-revalidate')->header('Pragma', 'no-cache')->header('Exprires', 'Sat 01 Jan 1990 00:00:00 GMT');
+        }
+      }
+
+    } catch (\Exception $e) {
+      //
+    }
+
+    $response = $next($request);
+    if ($response instanceof \Symfony\Component\HttpFoundation\BinaryFileResponse) {
+      return $response;
+    }
+
+    return $next($request)->header('Cache-control', 'no-control, no-store, max-age=0, must-revalidate')->header('Pragma', 'no-cache')->header('Exprires', 'Sat 01 Jan 1990 00:00:00 GMT');
+  }
 }
